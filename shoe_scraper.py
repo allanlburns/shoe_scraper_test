3cd162adf38152364a80305d50fbcc89776d55b1
Allan Burns <allanlburns@yahoo.com>
2020-06-23 15:09:49 -0400

    Scraping product name, price, and sku. Extracted code to function. NEXT: Enable scraper to start at home page and loop through products and select target product by sku.

--- shoe_scraper.py
+++ shoe_scraper.py
@@ -4,12 +4,20 @@ import requests
 from bs4 import BeautifulSoup
 from csv import writer
 
-response = requests.get("https://undefeated.com/collections/all/products/yeezy-boost-350-v2-citrin?variant=31506303975497")
-soup = BeautifulSoup(response.text, "html.parser")
-# prod_info = soup.find(class_="yada_wrap")
-prod_info = soup.select(".yada_wrap")
-# price = soup.find(class_="yada_wrap")[class_="money"]
+def product_info(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, "html.parser")
+    # prod_info = soup.find(class_="yada_wrap")
+    prod_info = soup.select(".yada_wrap")
+    prod_name = soup.find(class_="product-single__title").get_text()
+    price = soup.find(class_="money").get_text()
+    sku = soup.find(class_="variant-sku").get_text()
+    sku = sku.replace("-", "")
 
 
-print(prod_info)
-# print(price)
+    # print(prod_info)
+    print(f"Product Name: {prod_name}")
+    print(f"Price: {price}")
+    print(f"SKU: {sku}")
+
+product_info("https://undefeated.com/collections/all/products/yeezy-boost-350-v2-citrin?variant=31506303975497")
