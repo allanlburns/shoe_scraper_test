1a64aba5be448d6d690c71ab520e36ca4a2a15bb
Allan Burns <allanlburns@yahoo.com>
2020-07-05 17:01:29 -0400

    Defined to_discord method that sends JSON message to Discord webhook. Embed not working, just like in Postman. NEXT: Fix message embed and insert product info variables in to message text.

--- shoe_scraper.py
+++ shoe_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from csv import writer
 
@@ -11,7 +12,8 @@ def home_page_scanner(url):
     for link in products:
         product_info("http://undefeated.com/products/" + link["href"].split("/")[-1])
 
-
+        to_discord()
+        break
         # Continue through AJAX loop
 
         # next = soup.find(id="#AjaxinatePagination")
@@ -29,6 +31,10 @@ def product_info(url):
     # sku = sku.replace("-", "")
 
 
+
+
+
+
     # print(prod_info)
     print('\n')
     print('\n')
@@ -38,13 +44,41 @@ def product_info(url):
     print('\n')
     print('\n')
 
+
+def to_discord():
+
+    url = "https://discordapp.com/api/webhooks/729424811775885372/-DJsCvz0ZxZgWCpw9kQbb_ebEIjZ0UHLuIOkiOoy7yZCCNqBZD1nUT1r_Q6Qj3KBPBPf"
+
+    # JSON generated from Magic Bots' Embed generator
+    message = {
+        "embed": {
+            "author": {
+                "name": "Allan"
+            },
+            "title": "Test Embed Post",
+            "description": "Embed post coming from Magic Bots' Embed Generator Bot. Headed to the Shopify channel?"
+        },
+        "content": "Connected!!!"
+    }
+
+    jsonData = json.dumps(message)
+
+    newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    # jsonData = json.dumps(message)
+
+    response = requests.post(url, data=jsonData, headers=newHeaders)
+
+
+to_discord()
+
 # product_info("https://undefeated.com/collections/all/products/yeezy-boost-350-v2-citrin?variant=31506303975497")
 #
 # product_info("https://undefeated.com/collections/all/products/converse-x-pop-trading-co-jack-purcell-pro-ptc-hi-navy-citru?variant=31635394854985")
 
 
 # Scraping Undefeated
-home_page_scanner("https://undefeated.com/collections/all")
+# home_page_scanner("https://undefeated.com/collections/all")
 
 # Scraping Kith
 # home_page_scanner("https://kith.com/collections/kith-summer-2020")
