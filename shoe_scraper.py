00255a163c3a869005f1b80d84021e928203d899
Allan Burns <allanlburns@yahoo.com>
2020-07-05 16:27:48 -0400

    BS Scraper scrapes shoe info. NEXT: send info to Discord.

--- shoe_scraper.py
+++ shoe_scraper.py
@@ -1,23 +1,56 @@
-# Scraping Rithmschool Blog
-# https://www.rithmschool.com/blog
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
 
-def product_info(link):
-    response = requests.get(link)
+def home_page_scanner(url):
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    # prod_info = soup.find(class_="yada_wrap")
-    prod_info = soup.select(".yada_wrap")
+    # product_links = soup.select(".grid-product__meta")
+    products = soup.select(".grid-product__meta")
+
+    for link in products:
+        product_info("http://undefeated.com/products/" + link["href"].split("/")[-1])
+
+
+        # Continue through AJAX loop
+
+        # next = soup.find(id="#AjaxinatePagination")
+
+        # if next:
+        #     print(next)
+
+
+def product_info(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
     prod_name = soup.find(class_="product-single__title").get_text()
     price = soup.find(class_="money").get_text()
-    sku = soup.find(class_="variant-sku").get_text()
-    sku = sku.replace("-", "")
+    # sku = soup.find(class_="variant-sku").get_text()
+    # sku = sku.replace("-", "")
 
 
     # print(prod_info)
+    print('\n')
+    print('\n')
     print(f"Product Name: {prod_name}")
     print(f"Price: {price}")
-    print(f"SKU: {sku}")
+    # print(f"SKU: {sku}")
+    print('\n')
+    print('\n')
 
-product_info("https://undefeated.com/collections/all/products/yeezy-boost-350-v2-citrin?variant=31506303975497")
+# product_info("https://undefeated.com/collections/all/products/yeezy-boost-350-v2-citrin?variant=31506303975497")
+#
+# product_info("https://undefeated.com/collections/all/products/converse-x-pop-trading-co-jack-purcell-pro-ptc-hi-navy-citru?variant=31635394854985")
+
+
+# Scraping Undefeated
+home_page_scanner("https://undefeated.com/collections/all")
+
+# Scraping Kith
+# home_page_scanner("https://kith.com/collections/kith-summer-2020")
+
+# Scraping Juice
+# home_page_scanner("https://juicestoreusa.com/")
+
+# Scraping Saint Alfred
+# home_page_scanner("https://www.saintalfred.com/collections/brands")
---
+++ shoe_scraper2.py
@@ -0,0 +1,20 @@
+import scrapy
+import json
+
+class ShoeSpider(scrapy.Spider):
+    name = 'shoespider'
+    start_urls = ['https://undefeated.com/collections/all']
+
+    def parse(self, response):
+        for div in response.css('div.product_wrap '):
+
+            yield {
+                # 'price': article.css(".price_color::text").extract_first(),
+                # 'title': article.css("h3 > a::attr(title)").extract_first()
+                'name': div.css(".grid-product__title::text").extract_first(),
+                'price': div.css(".money::text").extract_first()
+            }
+
+            next = response.css('#AjaxinatePagination > a::attr(href)').extract_first()
+            if next:
+                yield response.follow(next, self.parse)
